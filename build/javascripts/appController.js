f6118224450de8f3a36046f0166133f2bf484fe4
cdeliens <cdeliens@gmail.com>
2013-09-27 12:14:03 -0600

    build update

--- build/javascripts/appController.js
+++ build/javascripts/appController.js
@@ -4,21 +4,20 @@
   app = angular.module("fireBubbleApp", ['firebase']);
 
   this.AppCtrl = function($scope, angularFire, angularFireCollection) {
-    var canvas, ctx, printBubble, ref_bubble;
+    var canvas, ctx, firebase_ref, printBubble;
     canvas = document.getElementById("main");
     ctx = canvas.getContext("2d");
-    ref_bubble = new Firebase('https://25este.firebaseio.com/bubble');
+    firebase_ref = new Firebase('https://25este.firebaseio.com/bubble');
     angularFire(ref_bubble, $scope, "bubble");
-    printBubble = function() {
+    firebase_ref.on('value', function(dataSnapshot) {
+      return printBubble();
+    });
+    return printBubble = function() {
       ctx.clearRect(0, 0, canvas.width, canvas.height);
       ctx.beginPath();
       ctx.arc($scope.bubble.x, $scope.bubble.y, 30, 0, 2 * Math.PI);
       return ctx.stroke();
     };
-    return setInterval(function() {
-      printBubble();
-      return 6000;
-    });
   };
 
 }).call(this);
